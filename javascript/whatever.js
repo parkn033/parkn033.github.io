85c4332579167021716f39b90094b24a80ffb998
Martin Park <parkn033@newschool.edu>
2016-10-13 20:52:57 +0000

    Adding page 2...

---
+++ javascript/whatever.js
@@ -0,0 +1,25 @@
+var age = 17; 
+var parentsPresent = false;
+
+if (age >= 18 || parentsPresent == true){
+    	console.log("Come on in..");
+} else {
+    if (age == 17) {
+        console.log("come back next year");
+    } else {
+        console.log("come back when you are 18, or bring your parents.");
+    }
+}
+
+console.log("test");
+
+var myName="Frank";
+
+if (myName == "frank"){
+    console.log("Your name is Frank");
+}
+
+if (age == 17){
+    console.log("come back next year");
+}
+
